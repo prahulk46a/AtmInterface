08136ebb069db0f225ee78a0e73c9178308bb939
Rahul Patil <98529455+prahulk46a@users.noreply.github.com>
2023-01-26 10:19:52 +0530

    Main method execution.

---
+++ NumberGuessing.java
@@ -0,0 +1,11 @@
+package com.NumberGuessing;
+public class NumberGuessing {
+
+	public static void main(String[] args) {
+		Method obj =new Method();//object created for class method 		
+		obj.run();
+
+
+	}
+
+}
